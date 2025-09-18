21e7c9ffa292e8dfa35fdc3e4de3182d99e8b44a
momo.agent <maxanit93@gmail.com>
2025-09-18 21:17:57 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+games 'gta5'
+
+author 'maxanit93'
+description 'fivem esx - anticl'
+
+client_scripts {
+    'client.lua',
+    'config.lua'
+}
+server_scripts {
+    'server.lua',
+    'config.lua'
+}
